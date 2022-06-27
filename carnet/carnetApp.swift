c43b0b1d6bd86b74f3a2554efa4aafb8ee09f325
mahalin <efmahalin@gmail.com>
2022-06-27 16:58:01 +0300

    Initial Commit

---
+++ carnet/carnetApp.swift
@@ -0,0 +1,17 @@
+//
+//  carnetApp.swift
+//  carnet
+//
+//  Created by Evgeniy Makhalin on 27.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct carnetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
