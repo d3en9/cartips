45d6c9e9fb4cc5ee386b5ae383572740f6242327
mahalin <efmahalin@gmail.com>
2022-08-04 18:52:52 +0300

    add color.swift

---
+++ carnet/Extensions/Color.swift
@@ -0,0 +1,27 @@
+//
+//  Color.swift
+//  carnet
+//
+//  Created by Evgeniy Makhalin on 15.07.2022.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+ static var theme: Color  {
+   return Color("theme")
+ }
+ static var BackgroundColor: Color  {
+   return Color("BackgroundColor")
+ }
+ static var BackgroundColorList: Color  {
+   return Color("BackgroundColorList")
+ }
+ static var TextColorPrimary: Color  {
+   return Color("TextColorPrimary")
+ }
+ static var TextColorSecondary: Color  {
+   return Color("TextColorSecondary")
+ }
+}
